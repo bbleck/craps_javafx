100eee627eb39277bf31e0f6e69c36e46c6d3d76
brianb <bbleck@unm.edu>
2018-10-18 10:17:58 -0600

    Implemented running functionality.

--- src/edu/cnm/deepdive/craps/controller/Controller.java
+++ src/edu/cnm/deepdive/craps/controller/Controller.java
@@ -2,13 +2,20 @@ package edu.cnm.deepdive.craps.controller;
 
 import edu.cnm.deepdive.craps.model.Game;
 import edu.cnm.deepdive.craps.model.Game.Roll;
+import edu.cnm.deepdive.craps.model.Game.State;
+import edu.cnm.deepdive.craps.view.RollCell;
 import java.security.SecureRandom;
 import java.util.List;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.css.Style;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
 import javafx.scene.text.Text;
+import javafx.util.Callback;
 
 public class Controller {
 
@@ -17,7 +24,7 @@ public class Controller {
   private String tallyFormat;
 
   @FXML
-  private ListView rolls;
+  private ResourceBundle resources;//this must be named "resources", if you've got this then fxml will autowire it
   @FXML
   private MenuItem fast;
   @FXML
@@ -28,11 +35,22 @@ public class Controller {
   private Text tally;
   @FXML
   private MenuItem next;
+  @FXML
+  private ListView<Roll> rolls;
 
   @FXML
-  private void initialize(){
+  private void initialize() {
     tallyFormat = tally.getText();//store the format string
+//    rolls.setCellFactory(new Callback<ListView<Roll>, ListCell<Roll>>() {  //anonymous class method, lambda method below
+//      @Override
+//      public ListCell<Roll> call(ListView<Roll> param) {
+//        return new RollCell(resources);
+//      }
+//    });
+    rolls.setCellFactory((lv) -> new RollCell(resources));
+    rolls.getStyleClass().add("");
     reset(null);
+    updateMenu();
   }
 
   @FXML
@@ -45,14 +63,14 @@ public class Controller {
   @FXML
   private void fast(ActionEvent actionEvent) {
     running = true;
-    //todo: update menu items enabled/disabled
-    //todo: instantiate and start runner
+    updateMenu();
+    new Runner().start();
   }
 
   @FXML
   private void pause(ActionEvent actionEvent) {
     running = false;
-    //todo: release runner instance
+
   }
 
   @FXML
@@ -62,13 +80,69 @@ public class Controller {
     updateRolls(game.getRolls());
   }
 
-  private void updateTally(int wins, int losses){
+  private void updateTally(int wins, int losses) {
     int total = wins + losses;
-    double percentage = (total != 0) ? 100 * wins / total : 0;
+    double percentage = (total != 0) ? 100.0 * wins / total : 0;
     tally.setText(String.format(tallyFormat, wins, total, percentage));
   }
 
-  private void updateRolls(List<Roll> rolls){
-    //todo: rolls listview
+  private void updateRolls(List<Roll> diceRolls) {
+    List<String> styleClasses = rolls.getStyleClass();
+    styleClasses.remove(styleClasses.size() - 1);
+    rolls.getItems().clear();
+    if (!diceRolls.isEmpty()) {
+      State state = diceRolls.get(diceRolls.size() - 1).getState();
+      if (state == State.WIN) {
+        styleClasses.add("win");
+      } else if (state == State.LOSS) {
+        styleClasses.add("loss");
+      }
+      rolls.getItems().addAll(diceRolls);
+    } else {
+      styleClasses.add("");
+    }
   }
+
+  private void updateMenu() {
+    next.setDisable(running);
+    fast.setDisable(running);
+    pause.setDisable(!running);
+    reset.setDisable(running);
+  }
+
+  private class Runner extends Thread {
+
+    private static final int TALLY_UPDATE_INTERVAL = 2000;
+    private static final int ROLLS_UPDATE_INTERVAL = 10_000;
+
+    @Override
+    public void run() {
+      int count = 0;
+      while (running) {
+        game.play();
+        count++;
+        if (count % TALLY_UPDATE_INTERVAL == 0) {
+          int wins = game.getWins();
+          int losses = game.getLosses();
+          Platform.runLater(() -> updateTally(wins,
+              losses));//this lambda dow not use a semicolon after update tally because there is just a single statement
+        }
+        if (count % ROLLS_UPDATE_INTERVAL == 0) {
+          var rolls = game.getRolls();//var is ok to use for a local variable, in java10 and higher
+          Platform.runLater(() -> updateRolls(rolls));
+        }
+      }
+      var wins = game.getWins();
+      var losses = game.getLosses();
+      var rolls = game.getRolls();
+      Platform.runLater(() -> {
+        updateTally(wins, losses); //local variables used in lambdas must be final; local variables CANNOT shadow other local variables
+        updateRolls(rolls);
+        updateMenu();
+      });
+
+
+    }
+  }
+
 }
